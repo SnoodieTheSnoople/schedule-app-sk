64dca96d5641733c379f7cde38c427602c4a4dcd
Cleef Gulfan <cleefgulfan312@outlook.com>
2024-01-01 20:42:49 +0000

    First page setup.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+
+</script>
+
+<svelte:head>
+	Homepage
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
