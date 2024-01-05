d142634c96cdba95df0ce90ea1b8208b4ff8f53b
Cleef <cleefgulfan312@gmail.com>
2024-01-05 18:25:15 +0000

    Cards for availability page made.

---
+++ src/lib/SecondaryCard.svelte
@@ -0,0 +1,51 @@
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let title;
+
+	/**
+	 * @type {string}
+	 */
+	export let left_content;
+
+	/**
+	 * @type {string}
+	 */
+	export let right_content;
+
+	/**
+	 * @type {string}
+	 */
+	export let bottom_content;
+</script>
+
+<div class="card w-full h-20 grid grid-cols-10 border-2 border-accent bg-base-100">
+	<div class="rounded-tl-lg rounded-bl-lg border-r-2 border-black col-span-2 bg-accent flex items-center justify-center">
+		<div class="card-title">
+			<h2 class="text-white">{title}</h2>
+		</div>
+	</div>
+	<div class="col-span-7 grid grid-cols-2 justify-center items-center">
+		{#if left_content && right_content}
+		<span class="pl-4 col-span-1">
+			<b>Available From:</b> {left_content}
+		</span>
+		<span class="pl-4 col-span-1">
+			<b>Available To:</b> {right_content}
+		</span>
+
+		<span class="pl-4 col-span-1">
+			<b>Preferred Shift:</b> {bottom_content}
+		</span>
+
+		{:else}
+		<span class="col-span-2 text-center">
+			No availability for this day.
+		</span>
+		{/if}
+	</div>
+		<div class="col-span-1 flex items-center justify-center">
+			<button class="btn bg-accent text-white">VIEW</button>
+		</div>
+</div>
--- src/routes/availability/+page.svelte
+++ src/routes/availability/+page.svelte
@@ -1,15 +1,16 @@
 <script>
-import Card from '$lib/Card.svelte';
+import SecondaryCard from '$lib/SecondaryCard.svelte';
 </script>
 
 <div class="mx-auto p-8 space-y-8 w-full h-full bg-white">
 	<h1 class="h1 font-bold">AVAILABILITY</h1>
+	<button class="btn btn-primary">New Availability</button>
 
-	<Card title="MONDAY"/>
-	<Card title="TUESDAY"/>
-	<Card title="WEDNESDAY"/>
-	<Card title="THURSDAY" left_content="13:00" right_content="20:00"/>
-	<Card title="FRIDAY" left_content="13:00" right_content="20:00"/>
-	<Card title="SATURDAY" left_content="13:00" right_content="20:00"/>
-	<Card title="SUNDAY" left_content="13:00" right_content="20:00"/>
+	<SecondaryCard title="MONDAY"/>
+	<SecondaryCard title="TUESDAY"/>
+	<SecondaryCard title="WEDNESDAY"/>
+	<SecondaryCard title="THURSDAY" left_content="13:00" right_content="20:00"/>
+	<SecondaryCard title="FRIDAY" left_content="13:00" right_content="20:00"/>
+	<SecondaryCard title="SATURDAY" left_content="13:00" right_content="20:00"/>
+	<SecondaryCard title="SUNDAY" left_content="13:00" right_content="20:00"/>
 </div>
