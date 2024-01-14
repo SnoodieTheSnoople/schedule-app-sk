a7d52d5b97916cb4a5e4dc0fcf92f25497d08e66
Cleef Gulfan <cleefgulfan312@outlook.com>
2024-01-14 01:11:29 +0000

    Refactor - Moved and created  commands file in src/lib.

--- src/routes/manager/calendar/[title]/+page.server.js
+++ src/routes/manager/calendar/[title]/+page.server.js
@@ -1,4 +1,3 @@
-import { supabase } from '$lib/supabaseClient.js';
 import { getAvailabilities, getUsersAndNames } from '$lib/supabaseCommands.js';
 
 export async function load({ params }) {
@@ -17,35 +16,3 @@ export async function load({ params }) {
 	};
 
 }
-
-/*
-async function getUsersAndNames() {
-	const { data, error } = await supabase.from("employees").select(`
-	id,
-	users(firstname, surname)|inner(id)`);
-
-	if (error) {
-		throw error;
-	}
-
-	return data;
-}
-
-async function getAvailabilities(arg) {
-	const { data, error } = await supabase.from("availabilities").select( `
-	day,
-	available_time_from,
-	available_time_to,
-	preferred_time_from,
-	preferred_time_to,
-	emp_id,
-	employees(id)|inner(id)`).eq('day', arg);
-
-	if (error) {
-		throw error;
-	}
-
-	return data;
-}
-
- */
